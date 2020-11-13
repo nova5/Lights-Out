751a42cc8cc0aaf97a05b75acd332f26e464d414
Nick Crowther <nick.crowther@pgl.co.uk>
2020-11-13 14:07:17 +0000

    UI / gameplay

--- LightsOut/LightsOutPuzzle.cs
+++ LightsOut/LightsOutPuzzle.cs
@@ -6,18 +6,31 @@ namespace LightsOut
 {
     public class LightsOutPuzzle
     {
-        public int NumberOfRows { get; private set; } = 5;
-        public int NumberOfCols { get; private set; } = 5;
+        public int NumberOfRows { get; private set; }
+        public int NumberOfCols { get; private set; }
 
         public bool[,] Board { get; set; }
 
-        public LightsOutPuzzle()
+        public bool Completed
         {
-            CreateBoard();
+            get
+            {
+                return IsPuzzleCompleted();
+            }
         }
 
-        public void CreateBoard()
+        //public LightsOutPuzzle()
+        //{
+        //    CreateBoard();
+        //}
+
+        public void CreateBoard(int numberOfRows, int numberOfCols)
         {
+            // range checking here
+
+            NumberOfRows = numberOfRows;
+            NumberOfCols = numberOfCols;
+
             var board = new bool[NumberOfRows, NumberOfCols];
             var rand = new Random();
 
@@ -40,13 +53,13 @@ namespace LightsOut
             }
 
             Board[x,y] = !Board[x, y];
-            if (x > 0) { Board[0, y] = !Board[0, y]; }
-            if (x < NumberOfRows-1) { Board[x+1, y] = !Board[x + 1, y]; }
-            if (y > 0) { Board[x, 0] = !Board[x, 0]; }
-            if (x < NumberOfCols-1) { Board[x, y+1] = !Board[x, y + 1]; }
+            if (x > 0) { Board[x-1, y] = !Board[x-1, y]; }
+            if (x < NumberOfRows-1) { Board[x+1, y] = !Board[x+1, y]; }
+            if (y > 0) { Board[x, y-1] = !Board[x, y-1]; }
+            if (y < NumberOfCols-1) { Board[x, y+1] = !Board[x, y+1]; }
         }
 
-        public bool PuzzleCompleted()
+        private bool IsPuzzleCompleted()
         {
             var lightsStillOn = false;
 
@@ -59,6 +72,27 @@ namespace LightsOut
             }
 
             return !lightsStillOn;
+        }
+
+        public void Cheat()
+        {
+            for (int x = 0; x < NumberOfRows; x++)
+            {
+                for (int y = 0; y < NumberOfCols; y++)
+                {
+                    Board[x, y] = false;
+                }
+            }
+
+            var centreRow = NumberOfRows / 2;
+            var centreCol = NumberOfCols / 2;
+
+            Board[centreRow, centreCol] = true;
+            Board[centreRow-1, centreCol] = true;
+            Board[centreRow+1, centreCol] = true;
+            Board[centreRow, centreCol-1] = true;
+            Board[centreRow, centreCol+1] = true;
+
         }
     }
 }
